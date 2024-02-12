251cd8c2f05d7baffbae2c5b55ac3f09b3f8ccd4
muthukumar-kanini <muthukumarv.kanini@gmail.com>
2024-02-12 17:04:26 +0530

    removed voice feature and added text input as a feature

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,67 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, Keyboard } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Features from '../components/Features';
 import { dummyMessages } from '../constants';
 import recordingIcon from '../../assets/recordingIcon.png';
-import {  TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import voiceLoading from '../../assets/voiceLoading.gif';
 import Voice from '@react-native-community/voice';
 
-
 export default function HomeScreen() {
     const [message, setMessage] = useState(dummyMessages);
-    const [recording , setRecording] = useState(false)//png
-    const [speaking , setSpeaking] = useState(true); //stop
+    const [recording, setRecording] = useState(false);
+    const [speaking, setSpeaking] = useState(true);
+    const [userInput, setUserInput] = useState('');
 
-    const SpeechStartHandler = e=>{
-        console.log('started ')
-    }
+    const handleInputChange = (text) => {
+        setUserInput(text);
+    };
 
-    const SpeechEndHandler = e=>{
-        setRecording(false)
-     
-        console.log('ended ')
-    }
-
-    const SpeechResultsHandler = e=>{
-        setRecording(false)
-        console.log('voice eveby ',e)
-    }
-   
-
-    const  startRecording  = async() =>{
-        setRecording(true)
-        try{
-            await Voice.start('en-GB')
-        }catch(error){
-            console.log(error)
+    const handleSend = () => {
+        if (userInput.trim() !== '') {
+            setMessage([...message, { role: 'user', content: userInput.trim() }]);
+            console.log(userInput.trim()); // Console log the typed value
+            setUserInput('');
+            Keyboard.dismiss(); // Hide the keyboard after sending message
         }
-    }
-
-    const  stopRecording  = async() =>{
-       
-        try{
-            await Voice.stop()
-            setRecording(false)
-        }catch(error){
-            console.log(error)
-        }
-    }
-    useEffect(() => {
-      Voice.onSpeechStart = SpeechStartHandler;
-      Voice.onSpeechEnd = SpeechEndHandler
-      Voice.onSpeechResults =SpeechResultsHandler
-
-      return ()=>{
-        Voice.destroy().then(Voice.removeAllListeners)
-      }
-    }, []);
-
-    const stopSpeaking = () =>{
-        setSpeaking(false)
-    }
+    };
 
     return (
         <View className="flex-1 bg-white">
@@ -70,100 +35,80 @@ export default function HomeScreen() {
                     <Image source={require('../../assets/bot.png')} style={{ height: hp(15), width: hp(15) }} />
                 </View>
 
-                {
-                    message.length > 0 ? (
-                        <View className="flex-1 space-y-3">
-                            <Text style={{ fontSize: wp(5) }} className="ml-2 font-semibold text-grey-700">Assistant</Text>
-                            <View style={{ height: hp(60) }} className="bg-neutral-200 rounded-3xl p-4 ">
-                                <ScrollView bounces={false} showsVerticalScrollIndicator={false} className="space-y-4">
-                                    {
-                                        message.map((messages, index) => {
-                                            if (messages.role === 'assistant') {
-                                                if (messages.content.includes('https')) {
-                                                    // Code to display an image
-                                                    return(
-                                                        <View  key={index} className='flex-row justify-start'>
-                                                           
-                                                            <View className=" bg-emerald-100 rounded-xl p-3 rounded-tl-none ">
-                                                            <Image source={{ uri: messages.content}} style={{height : wp(60) ,width: wp(60)}} className="rounded-2xl "/>
+                {/* Chat History */}
+                <View style={{ flex: 1 }}>
+                    {
+                        message.length > 0 ? (
+                            <View style={{ flex: 1 }}>
+                                <Text style={{ fontSize: wp(5) }} className="ml-2 font-semibold text-grey-700">Assistant</Text>
+                                <View style={{ height: hp(60) }} className="bg-neutral-200 rounded-3xl p-4 ">
+                                    <ScrollView bounces={false} showsVerticalScrollIndicator={false} className="space-y-4">
+                                        {
+                                            message.map((messages, index) => {
+                                                if (messages.role === 'assistant') {
+                                                    if (messages.content.includes('https')) {
+                                                        return (
+                                                            <View key={index} className='flex-row justify-start'>
+                                                                <View className=" bg-emerald-100 rounded-xl p-3 rounded-tl-none ">
+                                                                    <Image source={{ uri: messages.content }} style={{ height: wp(60), width: wp(60) }} className="rounded-2xl " />
+                                                                </View>
                                                             </View>
-                                                        </View>
-                                                    )
+                                                        )
+                                                    } else {
+                                                        return (
+                                                            <View key={index} className="flex-row ">
+                                                                <View>
+                                                                    <Text className="bg-emerald-100 rounded-xl p-3 rounded-tl-none">
+                                                                        {messages.content}
+                                                                    </Text>
+                                                                </View>
+                                                            </View>
+                                                        );
+                                                    }
                                                 } else {
-                                                    // Code to display text content
                                                     return (
-                                                        <View key={index} className="flex-row ">
+                                                        <View key={index} className="flex-row justify-end">
                                                             <View>
-                                                                <Text className="bg-emerald-100 rounded-xl p-3 rounded-tl-none">
+                                                                <Text className="bg-white rounded-xl p-3 rounded-tr-none">
                                                                     {messages.content}
                                                                 </Text>
                                                             </View>
                                                         </View>
                                                     );
                                                 }
-                                            } else {
-                                                // user 
-                                                return (
-                                                    <View key={index} className="flex-row justify-end">
-                                                        <View>
-                                                            <Text className="bg-white rounded-xl p-3 rounded-tr-none">
-                                                                {messages.content}
-                                                            </Text>
-                                                        </View>
-                                                    </View>
-                                                );
-                                            }
-                                        })
-                                    }
-                                </ScrollView>
+                                            })
+                                        }
+                                    </ScrollView>
+                                </View>
                             </View>
-                        </View>
-                    ) : (
-                        <Features />
-                    )
-                }
+                        ) : (
+                            <Features />
+                        )
+                    }
+                </View>
 
-
-                {/* recording features starts  */}
-                <View className="flex justify-center items-center ">
-                   {
-                         recording ? (
-                            <TouchableOpacity onPress={stopRecording}>
-                            <Image source={voiceLoading}  style={{width: wp(20) , height:wp(20)}} className="rounded-full mt-6"/>
+                {/* Input Form */}
+                <View style={{ padding: 10 }}>
+                    <TextInput
+                        className="bg-white"
+                        value={userInput}
+                        onChangeText={handleInputChange}
+                        placeholder="Type here..."
+                        style={{ borderWidth: 1, borderColor: 'gray', borderRadius: 5, padding: 10, marginBottom: 10, }}
+                    />
+                    <TouchableOpacity className="bg-emerald-100"  onPress={handleSend} style={{  borderRadius: 10, padding: 10 ,}}>
+                        <Text style={{ fontWeight: 'bold' }}>Send</Text>
+                    </TouchableOpacity>
+                    {
+                        message.length > 0 && (
+                            <TouchableOpacity className="top-1 mr-2" style={{ position: 'absolute', right: 10, padding: 10, borderRadius: 20, backgroundColor: '#CBD5E0', marginTop: 10 }} onPress={() => setMessage([])}>
+                                <Text style={{ color: 'white', fontWeight: 'bold' }}>Clear</Text>
                             </TouchableOpacity>
-                         )  :  
-
-                         (
-                            <TouchableOpacity onPress={startRecording}>
-                            <Image source={recordingIcon}  style={{width: wp(20) , height:wp(20)}} className="rounded-full mt-6"/>
-                            </TouchableOpacity>
-                         )
-                   }
-
-
-                   {/* clear button  */}
-                   {
-                    message.length > 0  && (
-                        <TouchableOpacity className="absolute right-10 p-3 rounded-3xl bg-neutral-400 mt-4"  onPress={() => setMessage([ ])}>
-                            <Text className="text-white font-semibold " >Clear</Text>
-                        </TouchableOpacity>
-                    )
-                   }
-
-                   {/* stop button */}
+                        )
+                    }
                    
-                   {
-                    speaking  > 0  && (
-                        <TouchableOpacity className="absolute left-10 p-3 rounded-3xl bg-red-400 mt-4"  onPress={stopSpeaking}>
-                            <Text className="text-white font-semibold  " >stop</Text>
-                        </TouchableOpacity>
-                    )
-                   }
-                   
-
                 </View>
             </SafeAreaView>
         </View>
     );
 }
-
